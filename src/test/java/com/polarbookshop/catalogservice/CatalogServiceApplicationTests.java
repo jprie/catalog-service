b5ef972cec1dd64207629925ca97e1c53cf95558
Johannes Priebsch <johannes@priebsch.at>
2024-04-11 13:25:39 +0200

    add integration tests for RBAC

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,20 +1,80 @@
 package com.polarbookshop.catalogservice;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.polarbookshop.catalogservice.domain.Book;
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
+@Testcontainers
 class CatalogServiceApplicationTests {
+
+    private static KeycloakToken bjornTokens;
+    private static KeycloakToken isabelleTokens;
 
     @Autowired
     private WebTestClient webTestClient;
+
+    @Container
+    private static final KeycloakContainer keycloakContainer =
+    new KeycloakContainer("quay.io/keycloak/keycloak:23.0")
+            .withRealmImportFile("/test-realm-config.json");
+
+    @DynamicPropertySource
+    static void dymanicProperties(DynamicPropertyRegistry registry) {
+
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri",
+                () -> keycloakContainer.getAuthServerUrl() + "/realms/PolarBookshop");
+    }
+
+    @BeforeAll
+    static void generateAccessTokens() {
+        WebClient webClient = WebClient.builder()
+                .baseUrl(keycloakContainer.getAuthServerUrl()
+                        + "/realms/PolarBookshop/protocol/openid-connect/token")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .build();
+
+        isabelleTokens = authenticateWith("isabelle", "password", webClient);
+        bjornTokens = authenticateWith("bjorn", "password", webClient);
+
+    }
+
+    private static KeycloakToken authenticateWith(
+            String username,
+            String password,
+            WebClient webClient
+    ) {
+        return webClient
+                .post()
+                .body(BodyInserters.fromFormData("grant_type", "password") // password grant flow
+                        .with("client-id", "polar-test")
+                        .with("username", username)
+                        .with("password", password)
+                )
+                .retrieve()
+                .bodyToMono(KeycloakToken.class)
+                .block();
+    }
+
     @Test
     void contextLoads() {
     }
@@ -27,6 +87,7 @@ class CatalogServiceApplicationTests {
         webTestClient
                 .post()
                 .uri("/books")
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(isabelleTokens.accessToken()))
                 .bodyValue(expectedBook)
                 .exchange()
                 .expectStatus().isCreated()
@@ -34,6 +95,17 @@ class CatalogServiceApplicationTests {
                     assertThat(actualBook).isNotNull();
                     assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
                 });
+    }
+
+    private record KeycloakToken(String accessToken) {
+        /**
+         * Make Jackson use this Constructor when deserializing JSON
+         * @param accessToken
+         */
+        @JsonCreator
+        private KeycloakToken(@JsonProperty("accessToken") final String accessToken) {
+            this.accessToken = accessToken;
+        }
     }
 
 }
